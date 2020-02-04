46ce17994e06ead3065c689e09ba75fbd8fbea63
Darya Parfenovich <56834032+Ugyldig@users.noreply.github.com>
2020-02-04 21:08:50 +0300

    Add files via upload

---
+++ graduate/js/skript.js
@@ -0,0 +1,72 @@
+(function($) {
+
+  $.fn.menumaker = function(options) {
+      
+      var menu_css = $(this), settings = $.extend({
+        title: "Menu",
+        format: "dropdown",
+        sticky: false
+      }, options);
+
+      return this.each(function() {
+        cssmenu.prepend('<div id="menu_button">' + settings.title + '</div>');
+        $(this).find("#menu_button").on('click', function(){
+          $(this).toggleClass('menu_opened');
+          var mainmenu = $(this).next('ul');
+          if (mainmenu.hasClass('open')) { 
+            mainmenu.hide().removeClass('open');
+          }
+          else {
+            mainmenu.show().addClass('open');
+            if (settings.format === "dropdown") {
+              mainmenu.find('ul').show();
+            }
+          }
+        });
+
+        cssmenu.find('li ul').parent().addClass('has-sub');
+
+        multiTg = function() {
+          menu_css.find(".has-sub").prepend('<span class="submenu-button"></span>');
+          menu_css.find('.submenu_button').on('click', function() {
+            $(this).toggleClass('submenu_opened');
+            if ($(this).siblings('ul').hasClass('open')) {
+              $(this).siblings('ul').removeClass('open').hide();
+            }
+            else {
+              $(this).siblings('ul').addClass('open').show();
+            }
+          });
+        };
+
+        if (settings.format === 'multitoggle') multiTg();
+        else cssmenu.addClass('dropdown');
+
+        if (settings.sticky === true) cssmenu.css('position', 'fixed');
+
+        resizeFix = function() {
+          if ($( window ).width() > 768) {
+            cssmenu.find('ul').show();
+          }
+
+          if ($(window).width() <= 768) {
+            cssmenu.find('ul').hide().removeClass('open');
+          }
+        };
+        resizeFix();
+        return $(window).on('resize', resizeFix);
+
+      });
+  };
+})(jQuery);
+
+(function($){
+$(document).ready(function(){
+
+$("#menu_css").menumaker({
+   title: "Menu",
+   format: "multitoggle"
+});
+
+});
+})(jQuery);
